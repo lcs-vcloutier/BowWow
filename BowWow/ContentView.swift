a6dff65414cc89b78ac871e4f59016218a76084e
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-11-26 09:43:25 -0500

    app works and successfully pulls from json everything works

--- BowWow/ContentView.swift
+++ BowWow/ContentView.swift
@@ -8,11 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+    @State private var dogImage = UIImage()
     var body: some View {
         NavigationView {
             VStack {
-                Image("example")
+                Image(uiImage: dogImage)
                     .resizable()
                     .scaledToFit()
                     .clipShape(Capsule())
@@ -63,57 +63,57 @@ struct ContentView: View {
             
             // Now decode from JSON into an array of Swift native data types
             if let decodedDoggieData = try? JSONDecoder().decode(RandomDog.self, from: doggieData) {
-
+                
                 print("Doggie data decoded from JSON successfully")
                 print("URL is: \(decodedDoggieData.message)")
-
+                
                 // Now fetch the image at the address we were given
                 fetchImage(from: decodedDoggieData.message)
-
+                
             } else {
-
+                
                 print("Invalid response from server.")
             }
-
+            
         }.resume()
         
     }
     // Get the actual image data
     func fetchImage(from address: String) {
-
-        // 1. Prepare a URLRequest to send our encoded data as JSON
+        
+        // 1. Prepare a URL that points to the image to be loaded
         let url = URL(string: address)!
-
+        
         // 2. Run the request and process the response
         URLSession.shared.dataTask(with: url) { data, response, error in
-
-            // handle the result here – attempt to unwrap optional data provided by task
+            
+            // Handle the result here – attempt to unwrap optional data provided by task
             guard let imageData = data else {
-
+                
                 // Show the error message
                 print("No data in response: \(error?.localizedDescription ?? "Unknown error")")
-
+                
                 return
             }
-
+            
             // Update the UI on the main thread
             DispatchQueue.main.async {
-
+                
                 // Attempt to create an instance of UIImage using the data from the server
                 guard let loadedDog = UIImage(data: imageData) else {
-
+                    
                     // If we could not load the image from the server, show a default image
                     dogImage = UIImage(named: "example")!
                     return
                 }
-
+                
                 // Set the image loaded from the server so that it shows in the user interface
                 dogImage = loadedDog
-
+                
             }
-
+            
         }.resume()
-
+        
     }
 }
 
