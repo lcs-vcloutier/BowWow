6bb1d319838f7627db8e0a06b728eb2e9e0e5b6b
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-11-26 08:56:12 -0500

    Initial Commit

---
+++ BowWow/BowWowApp.swift
@@ -0,0 +1,17 @@
+//
+//  BowWowApp.swift
+//  BowWow
+//
+//  Created by Cloutier, Vincent on 2020-11-26.
+//
+
+import SwiftUI
+
+@main
+struct BowWowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
